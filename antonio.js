fc061f649518f6738ad346729d3eed04dc540294
master <anto.programacion@gmail.com>
2021-10-19 21:26:04 +0200

    antonio modif label

--- antonio.js
+++ antonio.js
@@ -1,29 +1,27 @@
 
 /* CHECKBOX - EDAD */
-var form_content = document.getElementById("form");
-var input = document.createElement ("input");
-var label = document.createElement ("label");
-label.for = "rango_menor";
-form_content.appendChild(label);
-input.className="form-container-content";
-input.type="checkbox";
-input.id="rango_menor";
-input.name="rango_menor";
-input.value="rango_menor";
-input="Tengo menos de 18 años";
-form_content.appendChild(input);
+var form_content = document.getElementById("form"); /* Me situo en el form del documento */
+var input = document.createElement ("input"); /* creo un nodo input  */
+var label = document.createElement ("label"); /* creo un nodo label  */
+label.for = "rango_menor"; /* Doy el atributo for a label  */
+form_content.appendChild(label); /* meto como hijo a label del form_content  */
+label="Tengo menos de 18 años"; /*  Le doy un valor string a label */
+input.type="checkbox"; /* introduzco los atributos  */
+input.id="rango_menor"; 
+input.name="rango_menor"; 
+input.value="rango_menor"; 
+form_content.appendChild(input); /*  pongo input como hijo de form_content */
 
 
 var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "rango_veintena";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Tengo entre 18 y 30 años años";
 input.type="checkbox";
 input.id="rango_veintena";
 input.name="rango_veintena";
 input.value="rango_veintena";
-input="Tengo entre 18 y 30 años años";
 form_content.appendChild(input);    
 
 
@@ -31,12 +29,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "rango_treintena";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Tengo entre 30 y 40 años";
 input.type="checkbox";
 input.id="rango_treintena";
 input.name="rango_treintena";
 input.value="rango_treintena";
-input="Tengo entre 30 y 40 años";
 form_content.appendChild(input);
 
 
@@ -44,12 +41,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "rango_centena";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Tengo más de 40 años";
 input.type="checkbox";
 input.id="rango_centena";
 input.name="rango_centena";
 input.value="rango_centena";
-input="Tengo más de 40 años";
 form_content.appendChild(input);
 
 /*CHECKBOX - TIPO JUEGO */
@@ -58,72 +54,66 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "belico";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Bélico";
 input.type="checkbox";
 input.id="belico";
 input.name="belico";
 input.value="belico";
-input="Bélico";
 form_content.appendChild(input);
 
 var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "accion";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Acción";
 input.type="checkbox";
 input.id="accion";
 input.name="accion";
 input.value="accion";
-input="Acción";
 form_content.appendChild(input);
 
 var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "plataforma";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Plataforma";
 input.type="checkbox";
 input.id="plataforma";
 input.name="plataforma";
 input.value="plataforma";
-input="Plataforma";
 form_content.appendChild(input);
 
 var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "deportivo";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Deportivo";
 input.type="checkbox";
 input.id="deportivo";
 input.name="deportivo";
 input.value="deportivo";
-input="Deportivo";
 form_content.appendChild(input);
 
 var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "arcade";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Arcade";
 input.type="checkbox";
 input.id="arcade";
 input.name="arcade";
 input.value="arcade";
-input="Arcade";
 form_content.appendChild(input);
 
 var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "estrategia";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Estrategia";
 input.type="checkbox";
 input.id="estrategia";
 input.name="estrategia";
 input.value="estrategia";
-input="Estrategia";
 form_content.appendChild(input);
 
 /*CHECKBOX - INFO MAIL */
@@ -132,12 +122,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "mail_si";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Si, acepto recibir información";
 input.type="checkbox";
 input.id="mail_si";
 input.name="mail_si";
 input.value="mail_si";
-input="Si, acepto recibir información";
 form_content.appendChild(input);  
 
 /*CHECKBOX TIPOS CONSOLA */
@@ -147,12 +136,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "uno";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Un tipo";
 input.type="checkbox";
 input.id="uno";
 input.name="uno";
 input.value="uno";
-input="Un tipo";
 form_content.appendChild(input); 
 
 
@@ -160,12 +148,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "dos";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="dos tipos";
 input.type="checkbox";
 input.id="dos";
 input.name="dos";
 input.value="dos";
-input="dos tipos";
 form_content.appendChild(input); 
 
 
@@ -174,12 +161,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "tres_omas";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Tres o mas";
 input.type="checkbox";
 input.id="tres_omas";
 input.name="tres_omas";
 input.value="tres_omas";
-input="Tres o mas";
 form_content.appendChild(input); 
 
 /*CHECKBOX - IDIOMAS*/
@@ -190,12 +176,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "español";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Español";
 input.type="checkbox";
 input.id="español";
 input.name="español";
 input.value="español";
-input="Español";
 form_content.appendChild(input); 
 
 
@@ -204,12 +189,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "ingles";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Inglés";
 input.type="checkbox";
 input.id="ingles";
 input.name="ingles";
 input.value="ingles";
-input="Inglés";
 form_content.appendChild(input); 
 
 
@@ -218,12 +202,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "frances";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Francés";
 input.type="checkbox";
 input.id="frances";
 input.name="frances";
 input.value="frances";
-input="Francés";
 form_content.appendChild(input); 
 
 
@@ -232,12 +215,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "aleman";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Alemán";
 input.type="checkbox";
 input.id="aleman";
 input.name="aleman";
 input.value="aleman";
-input="Alemán";
 form_content.appendChild(input); 
 
 
@@ -246,12 +228,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "chino";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Chino";
 input.type="checkbox";
 input.id="chino";
 input.name="chino";
 input.value="chino";
-input="Chino";
 form_content.appendChild(input); 
 
 
@@ -260,12 +241,11 @@ var input = document.createElement ("input");
 var label = document.createElement ("label");
 label.for = "ruso";
 form_content.appendChild(label);
-input.className="form-container-content";
+label="Ruso";
 input.type="checkbox";
 input.id="ruso";
 input.name="ruso";
 input.value="ruso";
-input="Ruso";
 form_content.appendChild(input); 
 
 /* INSERTAR - IMAGENES */
