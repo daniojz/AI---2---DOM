a55a434ea73f0d4ca8b6939a08dcb66616a86a12
Alvarten <61624751+alvarten@users.noreply.github.com>
2021-10-19 21:17:12 +0200

    Update 01_AlvaroUrruela.js

--- 01_AlvaroUrruela.js
+++ 01_AlvaroUrruela.js
@@ -1,15 +1,15 @@
 function crerarNodosHTML(){
     //variable en la que introduzco el HTML
     let textos = 
-        "<label for="nombre">Nombre:</label>"+
+        "<label for='nombre'>Nombre:</label>"+
         "<input type='text' name='nombre' id='nombre'/><br>"+
-        "<label for="dni">Dni:</label>"+
+        "<label for='dni'>Dni:</label>"+
         "<input type='text' name='dni' id='dni'/><br>"+
-        "<label for="apellidos">Apellidos:</label>"+
+        "<label for='apellidos'>Apellidos:</label>"+
         "<input type='text' name='apellidos' id='apellidos'/><br>"+
-        "<label for="telefono">Telefono:</label>"+
+        "<label for='telefono'>Telefono:</label>"+
         "<input type='tel' name='telefono' id='telefono'/><br>"+
-        "<label for="texto">Opinion</label>"+
+        "<label for='texto'>Opinion</label>"+
         "<textarea name='texto' id='texto'/>"
     //Añado el HTML creado en js al div de mi página
     div1.innerHTML = textos
