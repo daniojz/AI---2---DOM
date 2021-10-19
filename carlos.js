d8e658e031c2d205a0621d6d09b5a9d73c33ab4d
Carlos Ashley <hdpspain@gmail.com>
2021-10-19 21:09:31 +0200

    js

--- carlos.js
+++ carlos.js
@@ -1,1 +1,17 @@
-......
+function tipoJuegos(){
+    //variable en la que introduzco el HTML
+    let textos =
+        "<label for='puzzle'>Puzzle:</label>"+
+        "<input type='radio' name='puzzle' id='puzzle'/>"+
+
+        "<label for='action'>Action:</label>"+
+        "<input type='radio' name='action' id='action'/>"+
+
+        "<label for='primera_persona'>Primera persona:</label>"+
+        "<input type='radio' name='primera_persona' id='primera_persona'/>"+
+
+        "<label for='deportes'>Deportes</label>"+
+        "<input type='radio' name='deportes' id='deportes'/>" 
+    //Añado el HTML creado en js al div de mi página
+    div2.innerHTML = textos
+}
